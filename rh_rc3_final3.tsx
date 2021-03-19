9e3fcf7ce1b15f16998f1330359479ccc2ee93be
nojanoja <75990903+nojanoja@users.noreply.github.com>
2021-03-19 20:23:17 +0100

    Add files via upload

---
+++ rh_rc3_final3.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rh_rc3_final3" tilewidth="32" tileheight="32" tilecount="361" columns="19">
+ <image source="rh_tileset final3.png" width="608" height="608"/>
+ <tile id="32">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ rh_tileset final4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="rh_tileset final2" tilewidth="32" tileheight="32" tilecount="361" columns="19">
+ <image source="rh_tileset final2.png" width="608" height="608"/>
+</tileset>
